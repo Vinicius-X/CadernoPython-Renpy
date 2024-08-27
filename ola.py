113f2b6fcd6b35ae7ce48858b21eb31d7ce27692
Vinicius <160507786+Vinicius-X@users.noreply.github.com>
2024-08-27 11:30:31 -0300

    Add files via upload

---
+++ ola.py
@@ -0,0 +1,19 @@
+ola="olá mundão pra lá de bão"
+print(ola)
+
+"""
+criando duas váriaveis
+"""
+a=8
+b=12
+somar=a+b
+print("A soma é:",somar)
+
+sub=a-b
+print("A subtração é:",sub)
+
+mult=a*b
+print("A multiplicaçaõ é:", mult)
+
+div=a/b
+print("A divisão é:", div)
