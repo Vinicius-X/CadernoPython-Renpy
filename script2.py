01b0f57a3485b90cd60409d28de8936f5e9c50d1
Vinicius <160507786+Vinicius-X@users.noreply.github.com>
2024-09-03 10:42:20 -0300

    Add files via upload

---
+++ script2.py
@@ -0,0 +1,44 @@
+# Atividade 02 - Python
+
+"""
+Programador: Vinicius Sousa
+
+Programa para testar varias funções em python
+
+"""
+if 10>1:
+    print("Condição VERDADEIRA")
+else:
+    print("condição FALSA")
+
+x = str(3) # variavel tipo texto str
+y = int(3) # variavel tipo inteiro int
+z = float(3) # variavel do tipo real ponto flutuante
+
+print (x)
+print (y)
+print (z)
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+frutas = ["banana","maçã","mamão"]
+a, b, c, = frutas
+print(a)
+print(b)
+print(c)
+
+print(a, b, c)
+print(a + b + c)
+
+print("fim do programa")
+
+num1=5
+num2=6
+print(5+6)
+
+num1="5"
+num2="6"
+print(num1 + num2)
+
